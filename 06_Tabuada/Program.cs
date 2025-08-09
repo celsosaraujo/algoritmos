1f690991db3a50b148af23d804b0c6d5da70eff7
celso.saraujo <celso.saraujo@hotmail.com>
2025-08-08 21:55:54 -0300

    Loop da tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicando, produto;
+
+            Console.WriteLine("Digite um número inteiro");
+            multiplicando = int.Parse( Console.ReadLine() );
+
+            Console.WriteLine("TABUADA DO {0}", multiplicando);
+            for ( int multiplicador = 1; multiplicador <= 10 ; multiplicador++)
+            {
+                produto = multiplicando * multiplicador;
+                Console.WriteLine("{0} x {1} = {2} ", multiplicando, multiplicador, produto);
+            }
+
+        }
+    }
+}
