a7e06ad4745332b77e966f8278ba8986f25a4a7a
celso.saraujo <celso.saraujo@hotmail.com>
2025-08-11 20:02:40 -0300

    Ajustes das mensagens de textos utilizando Interpolação de String

--- 06_Tabuada/Program.cs
+++ 06_Tabuada/Program.cs
@@ -7,22 +7,26 @@ namespace _06_Tabuada
         static void Main(string[] args)
         {
             int multiplicando = 0, produto;
-            bool digitacaoInvalida = false;
+            bool digitacaoValida = false;
 
             // ! negação: inverte o valor booleano
             // !false = true
             // !true = false
-            while ( !digitacaoInvalida )
+            //while ( digitacaoValida == false )
+            while ( !digitacaoValida )
             { 
                 Console.WriteLine("Digite um número inteiro");
-                digitacaoInvalida = int.TryParse(Console.ReadLine(), out  multiplicando);
+                digitacaoValida = int.TryParse(Console.ReadLine(), out  multiplicando);
             }
 
-            Console.WriteLine("TABUADA DO {0}", multiplicando);
+            //Console.WriteLine("TABUADA DO {0}", multiplicando);
+            //Interpolação de String $
+            Console.WriteLine($"TABUADA DO {multiplicando}");
             for ( int multiplicador = 1; multiplicador <= 10 ; multiplicador++)
             {
                 produto = multiplicando * multiplicador;
-                Console.WriteLine("{0} x {1} = {2} ", multiplicando, multiplicador, produto);
+                //Console.WriteLine("{0} x {1} = {2} ", multiplicando, multiplicador, produto);
+                Console.WriteLine($"{multiplicando} x {multiplicador} = {produto} ");
             }
 
         }
