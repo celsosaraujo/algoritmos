1e59749fa9c3bf3e08189d5e35645f9527511657
celso.saraujo <celso.saraujo@hotmail.com>
2025-08-22 20:24:46 -0300

    Criação do Método CalculaDV

--- 08_ValidaCPF/Program.cs
+++ 08_ValidaCPF/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace _08_ValidaCPF
 {
@@ -26,47 +23,17 @@ namespace _08_ValidaCPF
             }
 
             // 3- Validas CPFs com todos os números iguais
-           /* if( cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" ||
-                cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" ||
-                cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999"                 
-            )*/
-            if( cpf.Distinct().Count() == 1)    
+           if( cpf.Distinct().Count() == 1)    
             {
                 Console.WriteLine("CPF inválido! Números repetidos não são permitidos");
                 return;
             }
 
-            //4 - Cálculo do 1º Digito verificador
-            int soma = 0;
-            char[] cpfVetor = cpf.ToCharArray();
-            
-            for (int i = 0; i < 9; i++) 
-            {
-                soma += int.Parse( cpfVetor[i].ToString() ) * (10-i);            
-            }
-            int resto = soma % 11;
+            //4 - Cálculo do 1º Digito verificador            
+            int digX = CalculaDV(cpf, 9, 10);
 
-            int digX = 0;
-            if(resto >= 2) 
-            {
-                digX = 11 - resto;             
-            }
-
-            //5 - Cálculo do 2º Digito Verificador
-            soma = 0;
-            //char[] cpfVetor = cpf.ToCharArray();
-
-            for (int i = 0; i < 10; i++)
-            {
-                soma += int.Parse(cpfVetor[i].ToString()) * (11 - i);
-            }
-            resto = soma % 11;
-
-            int digY = 0;
-            if (resto >= 2)
-            {
-                digY = 11 - resto;
-            }
+            //5 - Cálculo do 2º Digito Verificador            
+            int digY = CalculaDV(cpf, 10, 11);
 
             //6 - Comparar os dígitos
             if (
@@ -81,5 +48,25 @@ namespace _08_ValidaCPF
                 Console.WriteLine("CPF INVÁLIDO!");
             }
         }
+
+        public static int CalculaDV( string cpf, int qtdeNumeros, int peso )
+        {
+            int soma = 0;
+            char[] cpfVetor = cpf.ToCharArray();
+
+            for (int i = 0; i < qtdeNumeros; i++)
+            {
+                soma += int.Parse(cpfVetor[i].ToString()) * (peso - i);
+            }
+            int resto = soma % 11;
+
+            int digito = 0;
+            if (resto >= 2)
+            {
+                digito = 11 - resto;
+            }
+            return digito;
+        }
+
     }
 }
