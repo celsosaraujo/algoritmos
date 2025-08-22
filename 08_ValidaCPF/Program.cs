ceddf19189a033bb449d498203a7a3cd3f12840c
celso.saraujo <celso.saraujo@hotmail.com>
2025-08-21 22:16:50 -0300

    Utilização do Distinct e Count para verificar números iguais

--- 08_ValidaCPF/Program.cs
+++ 08_ValidaCPF/Program.cs
@@ -26,10 +26,11 @@ namespace _08_ValidaCPF
             }
 
             // 3- Validas CPFs com todos os números iguais
-            if( cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" ||
+           /* if( cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" ||
                 cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" ||
                 cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999"                 
-            )
+            )*/
+            if( cpf.Distinct().Count() == 1)    
             {
                 Console.WriteLine("CPF inválido! Números repetidos não são permitidos");
                 return;
